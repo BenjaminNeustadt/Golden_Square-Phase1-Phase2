0b66d93a181fb15edeefb88d03132fde7170b8c1
benjamin <b.james.neustadt@gmail.com>
2022-06-28 12:20:06 +0200

    re-arranged submodules placing everything from this chapter within 2 seperate modules: Phase_one/Phase_two: then breaking down each exercise into its respective chapter, and making sure each has a lib folder, and spec folder.

--- Phase_one:Testing_Bites/testing_methods_with_equality/spec/greet_spec.rb
+++ Phase_one:Testing_Bites/testing_methods_with_equality/spec/greet_spec.rb
@@ -2,9 +2,8 @@ require 'greet'
 
 RSpec.describe "greet method" do
   it "greet a given user" do
-    result = greet("John")
-    expect(result). to eq "Hello, John!"
+    result = greet("Eric")
+    expect(result). to eq "Hello, Eric!"
   end
 end
-
-  
+ 
